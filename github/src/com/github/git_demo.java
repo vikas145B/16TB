ccb8f14c998cd831c950e59424c198f705ecf461
vikas145B <110393753+vikas145B@users.noreply.github.com>
2022-08-01 17:55:48 +0530

    Testing team

--- github/src/com/github/git_demo.java
+++ github/src/com/github/git_demo.java
@@ -1,7 +1,11 @@
 package com.github;
 
-public class git_demo {
-
+public class git_demo 
+{
+public void m1()
+{
+	System.out.println("git hub");
+}
 	public static void main(String[] args) 
 	{
 	System.out.println("hello");
